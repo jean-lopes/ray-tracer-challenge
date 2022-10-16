b820079d344b3b8e9bf45eea71fad04c79766a9a
Jean Lopes <jean.lopes@hotmail.com.br>
2022-10-16 20:40:24 -0300

    refactor float equality to a simpler version

--- src/rtc/float.clj
+++ src/rtc/float.clj
@@ -1,22 +1,10 @@
 (ns rtc.float
   "Floating point functions")
 
-(def epsilon
+(def ^:dynamic *epsilon*
   "Constant used on float equality as threshold when accouting for precision errors"
-  (java.lang.Math/ulp (float 1.0)))
+  (float 1e-5))
 
 (defn eq
-  "Compare two floating point numbers within an accepted precision.
-  Default precision is `rtc.math/epsilon`"
-  ([a b]
-   (eq epsilon a b))
-  ([e a b]
-   (if (= a b)
-     true
-     (let [abs-a       (abs a)
-           abs-b       (abs b)
-           diff        (abs (- a b))
-           abs-a+abs-b (+ abs-a abs-b)]
-       (if (or (zero? a) (zero? b) (< abs-a+abs-b Float/MIN_NORMAL))
-         (< diff (* e Float/MIN_NORMAL))
-         (< (/ diff (min abs-a+abs-b Float/MAX_VALUE)) e))))))
+  ^Boolean [^Float a ^Float b]
+  (< (abs (- a b)) *epsilon*))
